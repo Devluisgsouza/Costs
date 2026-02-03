7dd0939d2e09755aa32fb78618840ca9a3dda190
Dev Luis <devlgsouza@gmail.com>
2026-02-02 21:36:56 -0300

    Feat form de novo projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,15 @@
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css';
+
+
+
 function NewProject() {
     return(
-        <h1>New Project page</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie o seu projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar projeto"/>
+        </div>
     )
 }
 
