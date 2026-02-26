c3082f434d97df600a413ba442113ce768e0bbf2
Dev Luis <devlgsouza@gmail.com>
2026-02-25 21:11:23 -0300

    Fix Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,30 +1,61 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import Container from './Container'
 
 import styles from './Navbar.module.css'
 import logo from '../../img/costs_logo.png'
 
-
 function Navbar() {
     return(
         <nav className={styles.navbar}>
             <Container>
-                <Link className={styles.imglogo} to="/">
-                    <img src={logo} alt="Costs" /><h1>COSTS</h1>
-                </Link>
+                <NavLink className={styles.imglogo} to="/">
+                    <img src={logo} alt="Costs" />
+                    <h1>COSTS</h1>
+                </NavLink>
+
                 <ul className={styles.list}>
                     <li className={styles.item}>
-                        <Link to="/">Home</Link>
+                        <NavLink 
+                          to="/" 
+                          className={({ isActive }) => 
+                            isActive ? `${styles.link} ${styles.active}` : styles.link
+                          }
+                        >
+                          Home
+                        </NavLink>
                     </li>
+
                     <li className={styles.item}>
-                        <Link to="/projects">Projetos</Link>
+                        <NavLink 
+                          to="/projects"
+                          className={({ isActive }) => 
+                            isActive ? `${styles.link} ${styles.active}` : styles.link
+                          }
+                        >
+                          Projetos
+                        </NavLink>
                     </li>
-                    
+
                     <li className={styles.item}>
-                        <Link to="/company">Empresa</Link>
+                        <NavLink 
+                          to="/company"
+                          className={({ isActive }) => 
+                            isActive ? `${styles.link} ${styles.active}` : styles.link
+                          }
+                        >
+                          Empresa
+                        </NavLink>
                     </li>
+
                     <li className={styles.item}>
-                        <Link to="/contact">Contato</Link>
+                        <NavLink 
+                          to="/contact"
+                          className={({ isActive }) => 
+                            isActive ? `${styles.link} ${styles.active}` : styles.link
+                          }
+                        >
+                          Contato
+                        </NavLink>
                     </li>
                 </ul>
             </Container>
